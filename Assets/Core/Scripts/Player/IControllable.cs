9c849cf3a10d78b1accc7012c3ac1f43ee1921ca
klaytronic <klaytoncheukgamedev@gmail.com>
2024-03-02 14:03:17 -0500

    New player prefab named "Player", works with PlayerInputActions

--- Assets/Core/Scripts/Player/IControllable.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public interface IControllable
-{
-	public Vector2 GetMovementInput();
-	public Vector2 GetAimInput();
-}
--- Assets/Core/Scripts/Player/Movement.cs
+++ Assets/Core/Scripts/Player/Movement.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor.Examples;
+using System;
+using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
@@ -11,40 +14,57 @@ public class Movement : MonoBehaviour
 	[PropertyTooltip("Max speed while slowed.")]
 	[SerializeField] float slowedSpeed = 1.5f;
 
+	[Header("Dash Metrics")]
+	[PropertyTooltip("The distance the player travels by dashing.")]
+	[SerializeField] float dashLength;
+	[PropertyTooltip("How long a dash takes to complete.")]
+	[SerializeField] float dashDuration;
+	[PropertyTooltip("The easing of the dash.")]
+	[SerializeField] AnimationCurve dashCurve;
+
 	Transform reticlePivot;
-
 	float currentSpeed;
-
 	Quaternion inputRotation;
 	Rigidbody2D rb;
 	Vector2 moveInput, moveVector, aimInput, lookVector;
-	IControllable playerController;
+	bool dashing;
+	IEnumerator DashSequence;
 
     // Start is called before the first frame update
     void Start()
     {
 		rb = GetComponentInChildren<Rigidbody2D>();
 		currentSpeed = maxSpeed;
-		playerController = GetComponent<IControllable>();
-
-		if (playerController == null)
-			Debug.LogError("No player controller found!");
 
 		reticlePivot = transform.Find("ReticlePivot");
 		if (reticlePivot == null)
 			Debug.LogError("No reticle pivot object foumd");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        moveInput = playerController.GetMovementInput();
-		aimInput = playerController.GetAimInput();
-    }
+	public void OnMovement(InputValue value)
+	{
+		moveInput = value.Get<Vector2>();
+	}
+
+	public void OnDash()
+	{
+		Debug.Log("Dash pressed");
+		// dashes are not interruptible
+		if (DashSequence != null) return;
+
+		dashing = true;
+		DashSequence = Dash();
+		StartCoroutine(DashSequence);
+	}
+
+	public void OnThrow(InputValue value)
+	{
+		aimInput = value.Get<Vector2>();
+	}
 
 	private void FixedUpdate()
 	{
-        DoMovement();
+		if (!dashing) DoMovement();
 		DoAim();
 	}
 
@@ -69,7 +89,7 @@ public class Movement : MonoBehaviour
 		}
 
 		// rotate the reticle to point in the aim direction
-		inputRotation = Quaternion.Euler(Vector3.forward * (Mathf.Atan2(lookVector.x, lookVector.y) * Mathf.Rad2Deg));
+		inputRotation = Quaternion.Euler(Vector3.forward * (Mathf.Atan2(lookVector.y, lookVector.x) * Mathf.Rad2Deg));
 		reticlePivot.transform.rotation = inputRotation;
 	}
 
@@ -80,5 +100,37 @@ public class Movement : MonoBehaviour
 
 		// apply velocity
 		rb.velocity = moveVector;
+	}
+
+	IEnumerator Dash()
+	{
+		// get movement vector
+		Vector2 dashDir = moveInput.normalized * dashLength;
+
+		// get start and end position of this dash
+		Vector2 startPos = rb.position;
+		Vector2 endPos = rb.position + dashDir;
+
+		// evaluate dash motion
+		Vector2 lastPos = startPos;
+		float time = 0;
+		while (time < dashDuration)
+		{
+			// find the next position to be at, apply speed needed to get there
+			Vector2 nextPos = Vector2.Lerp(startPos, endPos, dashCurve.Evaluate(time / dashDuration));
+
+			if (Time.timeScale != 0)
+				rb.velocity = (nextPos - lastPos) / Time.deltaTime;
+			else
+				rb.velocity = Vector2.zero;
+
+			time += Time.deltaTime;
+			lastPos = nextPos;
+			yield return null;
+		}
+
+		// dash is now completed.
+		dashing = false;
+		DashSequence = null;
 	}
 }
